5c959c5968c94878469a6e33f859ef0c20133230
IvyGAOWei2 <Wei.Gao2@lincolnuni.ac.nz>
2025-06-14 23:27:24 +1200

    add note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../note";
 
 function App (){
 
     return(
         <div>
             <Header />
-            <Note />
+            {
+                notes.map(item => (
+                    <Note key={item.key} title={item.title} content={item.content} />
+                ))
+            }
             <Footer />
         </div>
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
 
-function Note (){
+function Note ({title, content}){
 
     return( 
         <div className="note">
-            <h1 >title</h1>
-            <p >content</p>
+            <h1 >{title}</h1>
+            <p >{content}</p>
         </div>
     );
 }
